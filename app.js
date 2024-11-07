34f5aa4ac2b55aa9e42b8d7c4b2d9afe4dc6a165
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-07 16:02:58 +0100

    fix: message on home

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const postRouter = require("./routers/posts.js")
-const postHome = require("./routers/home.js")
+//const postHome = require("./routers/home.js")
 const notFoundMiddleware = require("./middlewares/notFound.js")
 const loggerMiddleware = require("./middlewares/logger.js")
 const errorMiddleware = require("./middlewares/error.js")
@@ -24,9 +24,11 @@ app.listen(PORT, (req, res) => {
 
 app.use(loggerMiddleware)
 
-
 app.use("/posts", postRouter)
-app.use("/", postHome)
+app.use("/", (req, res) => {
+    res.send("Per l'elenco dei post, aggiungi '/posts' all'url qui sopra!")
+})
+
 
 app.use(notFoundMiddleware)
 
--- routers/home.js
+++
@@ -1,5 +0,0 @@
-const home = (req, res) => {
-    res.send("Per l'elenco dei post, aggiungi '/posts' all'url qui sopra!")
-}
-
-module.exports = home
